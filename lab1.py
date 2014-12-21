4a4a751fc9327f48719fad9fb63c30249526e9ca
Daniel Sjövall <dsjovall@gmail.com>
2014-12-21 23:58:44 +0100

    Initial commit

---
+++ lab1.py
@@ -0,0 +1,51 @@
+import cmath
+import math
+
+Yi = complex((1/math.sqrt(2)), -(1/math.sqrt(2)))
+Yi = Yi * pow(10, 2)
+#Yi = Yi * pow(10, -2)
+print("Yi: " + str(Yi))
+
+Y1 = complex(1/950, 0)
+#Y1 = Y1 / pow(10, -2)
+print("Y1: " + str(Y1))
+
+Y2 = complex(1750, 480)
+Y2 = 1/Y2
+#Y2 = Y2 * pow(10, -2)
+print("Y2: " + str(Y2))
+
+Y3 = complex(0, (0.86 * pow(10, -3)))
+#Y3 = Y3 * pow(10, -2)
+print("Y3: " + str(Y3))
+
+Yp = complex(1/5.5, -(1/8.6))
+Yp = Yp * pow((1/10), 2)
+#Yp = Yp * pow(10, -2)
+#Yp = math.pow((1/2), 2) * Yp
+print("Yp: " + str(Yp))
+
+Ytot = Yi + Y1 + Y2 + Y3 + Yp
+#print("Ytot: " + str(Ytot))
+#print(cmath.polar(Ytot))
+Ytot = Ytot / pow(10, -2)
+print("Ytot: " + str(Ytot))
+print(cmath.polar(Ytot))
+
+U0 = cmath.exp(complex(0, -cmath.pi))/Ytot
+print("U0: " + str(cmath.polar(U0)))
+
+U = cmath.polar(U0)
+U = list(U)
+U[0] = U[0]/-10
+print("U: " + str(U))
+
+
+Ue = U[0]/math.sqrt(2)
+print("Ue: " + str(Ue))
+
+P = pow(Ue, 2)/5.5
+print("P: " + str(P))
+
+Q = pow(Ue, 2)/(8.6*pow(10, -3))
+print("Q: " + str(Q))
---
+++ lab2.py
@@ -0,0 +1,23 @@
+import math
+
+Rl = 50
+R3 = 4650
+R4 = 20000
+R5 = 75000
+R6 = 300
+Idss = 10 * pow(10, -3)
+Up = -3.5
+Ugsq = Up * (math.sqrt(0.001/Idss) - 1)
+Ra = (Rl*R6)/(Rl+R6)
+Rb = 1/((1/R3)+(1/R4)+(1/R5))
+print("Rb: " + str(Rb))
+print("Ugsq: " + str(Ugsq))
+
+
+
+Gm = ((-2*Idss)/Up)*(1-(Ugsq/Up))
+h21 = 100
+h11 = 2000
+
+Uut = -((Ra * Gm * h21 * Rb)/((Rb + h11) + (Ra * (1+h21))))
+print("Uut: " + str(Uut))
